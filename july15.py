ebab1689f3828e9b36b0c75376e04cfadbaff92c
saketh-26 <sakethreddy.kallepu@gmail.com>
2022-09-04 14:29:57 +0530

    testing

---
+++ july15.py
@@ -0,0 +1,14 @@
+import flask
+#creates the Flask application object,which contains data about \
+#the application and also methods that tell the application to do
+#certain tasks/actions
+from flask import Flask,render_template
+#print(dir(flask))
+app = Flask(__name__)
+@app.route('/saketh')
+def check():
+    return "Hello everyone"
+@app.route('/new')
+def new():
+    return render_template('index.html')
+app.run(host='0.0.0.0', port=80)
